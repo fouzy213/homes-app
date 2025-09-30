865b12a19e17b9ccd79afe79778a2a31ae25db9d
fouzy213 <chikouchefouzy@gmail.com>
2025-09-30 16:41:18 +0200

    add_component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule,HousingLocationComponent],
+  template: `
+    <section>
+<form>
+  <input type="text" placeholder="Filter by city">
+<button class="primary" type="button">Search</button>
+</form>
+    </section>
+    <section class="results">
+          </section>
+<app-housing-location></app-housing-location>
+
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
